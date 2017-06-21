e8b564c79d4c2df70486937caa2c3574375cc5c3
PBruk <phillip.bruk@gmail.com>
2017-06-21 09:52:19 -0400

    Initial file commit

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * NU Student Life Lite Theme Customizer
+ *
+ * @package NU_Student_Life_Lite
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function nu_student_life_lite_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'nu_student_life_lite_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function nu_student_life_lite_customize_preview_js() {
+	wp_enqueue_script( 'nu_student_life_lite_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'nu_student_life_lite_customize_preview_js' );
